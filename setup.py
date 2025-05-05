fde7dfba7d76ad4fe93447db1d1d86ccfd0a75bf
elfman2 <nicolas.valot@gmail.com>
2025-05-05 03:28:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='object_detection_metrics',
     packages=find_packages(),
+    package_dir={'': 'src'},
     version='0.1.0',
     description='Object detection metrics.',
     author='y',
